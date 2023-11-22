369878ce4dcad4505a03b62521091aa32d52571e
신준영 <jyshine3@gmail.com>
2023-11-22 14:26:45 +0900

    feat: 상품정보 표현

--- shop-demo/src/components/Card.vue
+++ shop-demo/src/components/Card.vue
@@ -1,6 +1,6 @@
 <script setup>
 import {defineProps} from "vue";
-
+import lib from "@/script/lib";
 const { item } = defineProps(['item']);
 
 </script>
@@ -11,6 +11,7 @@ const { item } = defineProps(['item']);
 <!--    <img :src="item.imgPath">-->
 <!--    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
     <div class="card-body">
+      <small class="discount badge bg-danger">{{lib.getNumberFormatted(item.discountPer)}}%</small>
       <p class="card-text">{{item.name}}</p>
       <div class="d-flex justify-content-between align-items-center">
 <!--        <div class="btn-group">-->
@@ -18,7 +19,8 @@ const { item } = defineProps(['item']);
 <!--          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>-->
 <!--        </div>-->
         <button class="btn btn-primary">구입하기</button>
-        <small class="text-body-secondary">{{item.price}}</small>
+        <small class="price text-body-secondary">{{lib.getNumberFormatted(item.price)}}</small>
+        <small class="real text-body-secondary">{{lib.getNumberFormatted(item.price - (item.price * item.discountPer/ 100))}} 원</small>
       </div>
     </div>
   </div>
