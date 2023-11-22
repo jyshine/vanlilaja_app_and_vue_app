c20119deab201e0e8f2bee17b8ae489572d24ef3
신준영 <jyshine3@gmail.com>
2023-11-22 14:15:27 +0900

    feat: 서버 조회 결과로 노출

--- shop-demo/src/components/Card.vue
+++ shop-demo/src/components/Card.vue
@@ -7,20 +7,33 @@ const { item } = defineProps(['item']);
 
 <template>
   <div class="card shadow-sm">
-    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+    <span class="img" :style="{backgroundImage: `url(${item.imgPath})`}"/>
+<!--    <img :src="item.imgPath">-->
+<!--    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
     <div class="card-body">
-      <p class="card-text">{{item}}</p>
+      <p class="card-text">{{item.name}}</p>
       <div class="d-flex justify-content-between align-items-center">
-        <div class="btn-group">
-          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-        </div>
-        <small class="text-body-secondary">9 mins</small>
+<!--        <div class="btn-group">-->
+<!--          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>-->
+<!--          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>-->
+<!--        </div>-->
+        <button class="btn btn-primary">구입하기</button>
+        <small class="text-body-secondary">{{item.price}}</small>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.card .img {
+  display: inline-block;
+  width: 100%;
+  height: 250px;
+  background-size: cover;
+  background-position: center;
+}
 
+.card .card-body .price {
+  text-decoration: line-through;
+}
 </style>
