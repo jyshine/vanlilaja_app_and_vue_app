25ee2a8a846062aba3e9c3fb8738cfafd1e25b76
신준영 <jyshine3@gmail.com>
2023-12-11 22:41:38 +0900

    ExceptionControllerAdvice

---
+++ shop-demo-backend/src/main/java/com/example/shopdemobackend/config/ExceptionControllerAdvice.java
@@ -0,0 +1,10 @@
+package com.example.shopdemobackend.config;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ExceptionControllerAdvice {
+
+
+}
+`
